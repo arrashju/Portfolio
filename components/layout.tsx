7320d6ac63acc736dfcc26c960c001d6e203a836
arrashju <justin.ar.rasheed@gmail.com>
2021-06-26 18:42:42 -0400

    added external link for meta tag image

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({children}) => {
         <Head>
           <title>My page title</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <meta property="og:image" content="/photo.png" />
+          <meta property="og:image" content="https://media-exp3.licdn.com/dms/image/C4E03AQHRHO9wzj7nsg/profile-displayphoto-shrink_800_800/0/1617503181613?e=1629936000&v=beta&t=65jS-DHoMMs2HQbKDCa2eQcADr_rHzC3OZyfV6U8R_M" />
         </Head>
         {children}
       </>
