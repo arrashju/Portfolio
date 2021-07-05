0a9bd3d5bb83515f92b5d2729d2c1087c37b65c7
arrashju <justin.ar.rasheed@gmail.com>
2021-07-05 14:08:00 -0700

    updated styling of button

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { useState, useEffect } from 'react'
 import useNavScroll from '../hooks/useNavScroll'
 import useWindowSize from '../hooks/useWindowSize'
+import { Parallax } from 'react-parallax';
 
 const Home = () => {
 
