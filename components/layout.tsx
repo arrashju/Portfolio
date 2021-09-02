8f0fde67e349ec403aa843072e2506d1cbbe2d31
arrashju <justin.ar.rasheed@gmail.com>
2021-09-02 06:30:23 -0400

    updated head image

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({children}) => {
         <Head>
           <title>Justin Ar-Rasheed</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <meta property="og:image" content="https://media-exp3.licdn.com/dms/image/C4E03AQHRHO9wzj7nsg/profile-displayphoto-shrink_800_800/0/1617503181613?e=1629936000&v=beta&t=65jS-DHoMMs2HQbKDCa2eQcADr_rHzC3OZyfV6U8R_M" />
+          <meta property="og:image" content="/photo_small.png" />
         </Head>
         {children}
       </>
